67f883b3bdcb6e1c9ac3bdd36540e845e8fa6eb9
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-09-24 08:58:38 +0500

    modified

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ btn.addEventListener('click', function () {
     var firstName = document.getElementById("firstName");
     var lastName = document.getElementById("lastName");
     var birthDay = document.getElementById("birthdayDate");
-    var phoneNumber = document.getElementById("phoneNumber");
+    var phoneNumber = document.getElementById("phoneNum");
     var password = document.getElementById("password");
     var emailAddress = document.getElementById("emailAddress");
     console.log(firstName.value,lastName.value,birthDay.value,phoneNumber.value,password.value,emailAddress.value)
@@ -27,7 +27,7 @@ btn.addEventListener('click', function () {
         emailAddress: emailAddress.value,
         password: password.value
     };
-
+    var users = JSON.parse(localStorage.getItem("usersData")) 
     users.push(userobj);
     localStorage.setItem('usersData', JSON.stringify(users));
     window.location.href = "login.html"
@@ -38,6 +38,13 @@ btn.addEventListener('click', function () {
     lastName.value = "";
     password.value = "";
 });
-
+var login=document.getElementById("login")
+login.addEventListener('click',()=>{
+    swal({
+        title: "Sweet!",
+        text: "Here's a custom image.",
+        imageUrl: "images/thumbs-up.jpg"
+      });
+})
 
 
